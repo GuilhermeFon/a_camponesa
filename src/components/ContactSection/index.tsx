c8fbc278c71a21eaec680f7ad0bd2e715bf469ba
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2025-08-21 15:24:55 -0300

    Refactor ContactSection layout and improve contact links for better accessibility

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -28,67 +28,94 @@ export default function ContactSection() {
           </p>
         </div>
 
-        <div className="bg-transparent shadow-none border-none mb-14 sm:mb-0 flex flex-col md:flex-row justify-center items-center gap-12 md:gap-32">
-          <div className="w-fit place-items-center">
-            <h3 className="text-2xl font-semibold text-camponesa-creme mb-6">
+        <div className="bg-transparent shadow-none border-none mb-14 sm:mb-0 flex flex-col md:flex-row justify-center items-center md:items-start gap-12 md:gap-32">
+          <div className="w-fit place-items-center flex flex-col space-y-4 items-center">
+            <h3 className="text-2xl font-semibold text-camponesa-creme">
               Funcionamento
             </h3>
-            <div className="flex flex-col space-y-4 items-center">
-              <div className="text-center">
-                <p className="text-camponesa-creme font-semibold">
-                  Segunda - Sexta
-                </p>
-                <p className="text-camponesa-creme">8:00 - 18:00</p>
-              </div>
-              <div className="text-center">
-                <p className="text-camponesa-creme font-semibold">Sabádo</p>
-                <p className="text-camponesa-creme">8:00 - 12:00</p>
-              </div>
+            <div className="text-center">
+              <p className="text-camponesa-creme font-semibold">
+                Segunda - Sexta
+              </p>
+              <p className="text-camponesa-creme">8:00 - 18:00</p>
+            </div>
+            <div className="text-center">
+              <p className="text-camponesa-creme font-semibold">Sabádo</p>
+              <p className="text-camponesa-creme">8:00 - 12:00</p>
             </div>
           </div>
 
-          <div className="w-fit">
-            <h3 className="text-2xl font-semibold text-camponesa-creme mb-6">
+          <div className="w-fit space-y-4">
+            <h3 className="text-2xl font-semibold text-camponesa-creme">
               Entre em Contato
             </h3>
-            <div className="space-y-4">
-              <div className="flex items-start space-x-3">
+
+            <div className="flex items-start space-x-3">
+              <a
+                href="https://share.google/ZRTSwo5x9uQzlISvw"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-camponesa-creme hover:text-camponesa-terracota transition-colors"
+              >
                 <MapPin className="text-camponesa-terracota mt-1" size={20} />
-                <div>
+              </a>
+              <div>
+                <a
+                  href="https://share.google/ZRTSwo5x9uQzlISvw"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-camponesa-creme hover:text-camponesa-terracota transition-colors"
+                >
                   <p className="text-camponesa-creme font-semibold">Endereço</p>
                   <p className="text-camponesa-creme">
                     Capão Raso, Curitiba - PR
                   </p>
-                </div>
+                </a>
               </div>
-              <div className="flex items-start space-x-3">
-                <Phone className="text-camponesa-terracota mt-1" size={20} />
-                <div>
-                  <p className="font-semibold text-camponesa-creme">Telefone</p>
-                  <p className="text-camponesa-creme">(41) 98460-2341</p>
-                </div>
-              </div>
+            </div>
 
-              <div className="flex items-center gap-4">
+            <div className="flex items-start space-x-3">
+              <a
+                href="https://wa.me/5541984602341?text=Olá! Gostaria de saber mais sobre os produtos da Camponesa."
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-camponesa-creme hover:text-camponesa-terracota transition-colors"
+              >
+                <Phone className="text-camponesa-terracota mt-1" size={20} />
+              </a>
+              <div>
                 <a
-                  href="https://wa.me/5541984602341"
+                  href="https://wa.me/5541984602341?text=Olá! Gostaria de saber mais sobre os produtos da Camponesa."
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label="WhatsApp"
-                  className="text-camponesa-terracota hover:text-camponesa-creme transition-colors"
+                  className="text-camponesa-creme hover:text-camponesa-terracota transition-colors"
                 >
-                  <WhatsappIcon size={24} />
-                </a>
-                <a
-                  href="https://www.instagram.com/acamponesaconfeitaria/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Instagram"
-                  className="text-camponesa-terracota hover:text-camponesa-creme transition-colors"
-                >
-                  <InstagramIcon size={20} />
+                  <p className="font-semibold text-camponesa-creme">Telefone</p>
+                  <p>(41) 98460-2341</p>
                 </a>
               </div>
+            </div>
+
+            <div className="flex items-start space-x-3">
+              <a
+                href="https://www.instagram.com/acamponesaconfeitaria/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram"
+                className="text-camponesa-terracota hover:text-camponesa-creme transition-colors"
+              >
+                <InstagramIcon size={20} />
+              </a>
+              <a
+                href="https://www.instagram.com/acamponesaconfeitaria/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Instagram"
+                className="text-camponesa-terracota hover:text-camponesa-creme transition-colors"
+              >
+                <p className="font-semibold text-camponesa-creme">Instagram</p>
+                <p className="text-camponesa-creme">@acamponesaconfeitaria</p>
+              </a>
             </div>
           </div>
         </div>
@@ -97,7 +124,8 @@ export default function ContactSection() {
       {/* Footer */}
       <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 text-white px-4 w-full">
         <div className="text-sm sm:text-sm text-camponesa-creme text-center flex flex-col sm:block">
-          <span>© 2025 A Camponesa | Todos os direitos reservados.</span>
+          <span>© 2025 A Camponesa | Todos os direitos reservados</span>
+          <span className="hidden sm:inline-block mx-2">|</span>
           <span>
             Desenvolvido por{" "}
             <a
